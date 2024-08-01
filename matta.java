26b2b34e2c1cbcaa0396f0b35f41104d577faae3
Your Name <your.email@example.com>
2024-08-01 21:18:18 +0530

    new project file adding

---
+++ matta.java
@@ -0,0 +1,13 @@
+public class matta {
+    void call(){
+        System.out.println("take a call");
+    }
+    void massage(){
+        System.out.println("type a massage");
+    }
+    public static void main(String[] args) {
+        matta ob = new matta();
+        ob.call();
+    }
+    
+}
